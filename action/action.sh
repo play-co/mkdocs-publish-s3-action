dbaeef9a0782fbc8f36f5f3f882cab5dfcc71cfb
Jake Davies <jgdavies24@gmail.com>
2020-07-31 11:33:55 -0700

    remove force

--- action/action.sh
+++ action/action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdocs build --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" --force
+mkdocs build --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" 
 ls "${GITHUB_WORKSPACE}/docsite/static"
 
 
