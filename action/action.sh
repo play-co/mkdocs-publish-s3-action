8f050c005ad68400f7741ce638ca343492917a07
Jake Davies <jgdavies24@gmail.com>
2020-08-04 12:07:10 -0700

    try actions in other order

--- action/action.sh
+++ action/action.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ "${TYPEDOC}" = "true" ]; then
+  typedoc "${GITHUB_WORKSPACE}/src" --out "${GITHUB_WORKSPACE}/docs/generated"
+fi
+
 mkdocs build --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" 
 
 
-if [ "${TYPEDOC}" = "true" ]; then
-  typedoc "${GITHUB_WORKSPACE}/src" --out "${GITHUB_WORKSPACE}/docsite/static/generated"
-fi
 
 aws s3 cp --recursive "${GITHUB_WORKSPACE}/docsite/static" $S3_PATH
 
