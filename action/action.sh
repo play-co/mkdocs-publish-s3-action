6d0995ba07de5969a57773e7968bf80ce11c679b
Jake Davies <jgdavies24@gmail.com>
2020-08-04 11:23:54 -0700

    add typedoc

--- action/action.sh
+++ action/action.sh
@@ -2,6 +2,11 @@
 set -e
 
 mkdocs build --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" 
+
+if [ "${TYPEDOC}" = "true" ]; then
+  typedoc "${GITHUB_WORKSPACE}/src" --out "${GITHUB_WORKSPACE}/docsite/static/generated"
+fi
+
 aws s3 cp --recursive "${GITHUB_WORKSPACE}/docsite/static" $S3_PATH
 
 
